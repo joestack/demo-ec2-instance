2948eca6e49282d0a1ed1e4dad6f7907188eef44
joestack <joern@joern-online.de>
2018-10-31 07:06:40 +0100

    init

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 0.11.0"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "${var.instance_type}"
+##  availability_zone = "${var.aws_region}a"
+  availability_zone = "${var.aws_region}"
+
+  tags {
+    Name = "${var.name}"
+  }
+}
