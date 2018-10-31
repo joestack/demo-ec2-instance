18f00c82774f801c6dfc754a42a4f07c9abd0e11
joestack <joern@joern-online.de>
2018-10-31 16:04:12 +0100

    demo run

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 0.11.0"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "${var.instance_type}"
+  availability_zone = "${var.aws_region}a"
+
+  tags {
+    Name = "${var.name}"
+  }
+}
